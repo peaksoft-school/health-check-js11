b7d67fa5c877d39c297f5e57e15f09e2606e7e61
Sheralieva Bermet <bermetsheralieva79@gmail.com>
2024-01-10 16:22:25 +0600

    add fonts and theme settings

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from '@emotion/react'
+import './index.css'
+import App from './App'
+import theme from './utils/theme/theme'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+   <StrictMode>
+      <ThemeProvider theme={theme}>
+         <App />
+      </ThemeProvider>
+   </StrictMode>
+)
