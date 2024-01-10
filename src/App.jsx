9033e4c7b2d41b0049c17c026062331c19bce8e2
Sheralieva Bermet <bermetsheralieva79@gmail.com>
2024-01-10 21:27:23 +0600

    theme settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import './App.css'
-import { styled } from '@mui/material/styles'
 
 function App() {
-   return <Div>health-check-js11</Div>
+   return <div>health-check-js11</div>
 }
 
 export default App
-
-const Div = styled('div')(({ theme }) => ({
-   color: theme.palette.primary.darkGreen,
-}))
