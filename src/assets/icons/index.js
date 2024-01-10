353076c6b27ba3647c07a4d86193a898ed6d9dba
BayturPS <bayturazarbekov@gmail.com>
2024-01-10 16:10:39 +0600

    added icons

---
+++ src/assets/icons/index.js
@@ -0,0 +1,71 @@
+import { ReactComponent as UsersIcon } from './svgs/users.svg'
+import { ReactComponent as ActivePhoneIcon } from './svgs/activePhone.svg'
+import { ReactComponent as ActiveRadioIcon } from './svgs/activeRadio.svg'
+import { ReactComponent as AddPhotoIcon } from './svgs/addPhoto.svg'
+import { ReactComponent as CalendarIcon } from './svgs/calendar.svg'
+import { ReactComponent as CalendarWithDotsIcon } from './svgs/calendarWithDots.svg'
+import { ReactComponent as CanceledRedgisterIcon } from './svgs/canceledRegister.svg'
+import { ReactComponent as CloseIcon } from './svgs/close.svg'
+import { ReactComponent as CloseEyesIcon } from './svgs/closeEyes.svg'
+import { ReactComponent as DefaultPhoneIcon } from './svgs/defaultPhone.svg'
+import { ReactComponent as DefaultRadioIcon } from './svgs/defaultRadio.svg'
+import { ReactComponent as DeleteIcon } from './svgs/delete.svg'
+import { ReactComponent as DownIcon } from './svgs/down.svg'
+import { ReactComponent as EditIcon } from './svgs/edit.svg'
+import { ReactComponent as FooterInstagramIcon } from './svgs/footerInstagram.svg'
+import { ReactComponent as FooterTelegramIcon } from './svgs/footerTelegram.svg'
+import { ReactComponent as FooterWhatsAppIcon } from './svgs/footerWhatsApp.svg'
+import { ReactComponent as HeaderInstagramIcon } from './svgs/headerInstagram.svg'
+import { ReactComponent as HeaderTelegramIcon } from './svgs/headerTelegram.svg'
+import { ReactComponent as HeaderWhatsAppIcon } from './svgs/headerWhatsApp.svg'
+import { ReactComponent as HourIcon } from './svgs/hour.svg'
+import { ReactComponent as LittleCloseIcon } from './svgs/littleClose.svg'
+import { ReactComponent as LocationIcon } from './svgs/location.svg'
+import { ReactComponent as OpenEyesIcon } from './svgs/openEyes.svg'
+import { ReactComponent as PlusIcon } from './svgs/plus.svg'
+import { ReactComponent as PrintIcon } from './svgs/print.svg'
+import { ReactComponent as ProfileIcon } from './svgs/profile.svg'
+import { ReactComponent as RefreshIcon } from './svgs/refresh.svg'
+import { ReactComponent as RegisteredIcon } from './svgs/registered.svg'
+import { ReactComponent as SaveIcon } from './svgs/save.svg'
+import { ReactComponent as SearchIcon } from './svgs/search.svg'
+import { ReactComponent as SmsIcon } from './svgs/sms.svg'
+import { ReactComponent as UpIcon } from './svgs/up.svg'
+import { ReactComponent as UserIcon } from './svgs/user.svg'
+
+export {
+   UsersIcon,
+   ActivePhoneIcon,
+   ActiveRadioIcon,
+   AddPhotoIcon,
+   CalendarIcon,
+   CalendarWithDotsIcon,
+   CanceledRedgisterIcon,
+   CloseEyesIcon,
+   CloseIcon,
+   DefaultPhoneIcon,
+   DefaultRadioIcon,
+   DeleteIcon,
+   DownIcon,
+   EditIcon,
+   FooterInstagramIcon,
+   FooterTelegramIcon,
+   FooterWhatsAppIcon,
+   HeaderInstagramIcon,
+   HeaderTelegramIcon,
+   HeaderWhatsAppIcon,
+   HourIcon,
+   LittleCloseIcon,
+   LocationIcon,
+   OpenEyesIcon,
+   PlusIcon,
+   PrintIcon,
+   ProfileIcon,
+   RefreshIcon,
+   RegisteredIcon,
+   SaveIcon,
+   SearchIcon,
+   SmsIcon,
+   UpIcon,
+   UserIcon,
+}
