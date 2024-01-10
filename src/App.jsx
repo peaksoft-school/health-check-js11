15ca1cbbf3648f72b4915fc4bd7a9aec1d831e53
BayturPS <bayturazarbekov@gmail.com>
2024-01-10 17:09:39 +0600

    added services icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 
 function App() {
-   return <div>Health Check</div>
+   return <div>Health-Check</div>
 }
 
 export default App
--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,3 +1,5 @@
+// Default icons===================================================
+
 import { ReactComponent as UsersIcon } from './svgs/users.svg'
 import { ReactComponent as ActivePhoneIcon } from './svgs/activePhone.svg'
 import { ReactComponent as ActiveRadioIcon } from './svgs/activeRadio.svg'
@@ -32,6 +34,29 @@ import { ReactComponent as SearchIcon } from './svgs/search.svg'
 import { ReactComponent as SmsIcon } from './svgs/sms.svg'
 import { ReactComponent as UpIcon } from './svgs/up.svg'
 import { ReactComponent as UserIcon } from './svgs/user.svg'
+
+// Service icons===================================================
+
+import { ReactComponent as AllergologyIcon } from './servicesIcons/allergology.svg'
+import { ReactComponent as AnesteziologiaIcon } from './servicesIcons/anesteziologiya.svg'
+import { ReactComponent as DermatologiyaIcon } from './servicesIcons/dermatologiya.svg'
+import { ReactComponent as EndokrinologiyaIcon } from './servicesIcons/endokrinologiya.svg'
+import { ReactComponent as FizioterapiyaIcon } from './servicesIcons/fizioterapiya.svg'
+import { ReactComponent as FlebologiyaIcon } from './servicesIcons/flebologiya.svg'
+import { ReactComponent as GastroenterologyIcon } from './servicesIcons/gastroenterology.svg'
+import { ReactComponent as GinekologiyaIcon } from './servicesIcons/ginekologiya.svg'
+import { ReactComponent as KardiologiyaIcon } from './servicesIcons/kardiologiya.svg'
+import { ReactComponent as NeurosurgeryIcon } from './servicesIcons/neurosurgery.svg'
+import { ReactComponent as NevrologiyaIcon } from './servicesIcons/nevrologiya.svg'
+import { ReactComponent as OftalmologiyaIcon } from './servicesIcons/oftalmologiya.svg'
+import { ReactComponent as OnkologiyaIcon } from './servicesIcons/onkologiya.svg'
+import { ReactComponent as OrtopediyaIcon } from './servicesIcons/ortopediya.svg'
+import { ReactComponent as OtorinolaringologiyaIcon } from './servicesIcons/otorinolaringologiya.svg'
+import { ReactComponent as ProktologiyaIcon } from './servicesIcons/proktologiya.svg'
+import { ReactComponent as RevmatologiyaIcon } from './servicesIcons/revmatologiya.svg'
+import { ReactComponent as TerapiyaIcon } from './servicesIcons/terapiya.svg'
+import { ReactComponent as UrologiyaIcon } from './servicesIcons/urologiya.svg'
+import { ReactComponent as VaccinationIcon } from './servicesIcons/vaccination.svg'
 
 export {
    UsersIcon,
@@ -68,4 +93,24 @@ export {
    SmsIcon,
    UpIcon,
    UserIcon,
+   AllergologyIcon,
+   AnesteziologiaIcon,
+   DermatologiyaIcon,
+   EndokrinologiyaIcon,
+   FizioterapiyaIcon,
+   FlebologiyaIcon,
+   GastroenterologyIcon,
+   GinekologiyaIcon,
+   KardiologiyaIcon,
+   NeurosurgeryIcon,
+   NevrologiyaIcon,
+   OftalmologiyaIcon,
+   OnkologiyaIcon,
+   OrtopediyaIcon,
+   OtorinolaringologiyaIcon,
+   ProktologiyaIcon,
+   RevmatologiyaIcon,
+   TerapiyaIcon,
+   UrologiyaIcon,
+   VaccinationIcon,
 }
