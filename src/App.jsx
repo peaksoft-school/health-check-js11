1523395b7ca6497f992d0c0f2bdef846ee1c41fb
Sheralieva Bermet <bermetsheralieva79@gmail.com>
2024-01-11 12:08:59 +0600

    clear App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
 import './App.css'
-import CheckBox from './components/UI/switchers/CheckBox'
-import UIRadio from './components/UI/switchers/UIRadio'
 
 function App() {
-   return (
-      <div>
-         health-check-js11
-         <CheckBox />
-         <UIRadio />
-      </div>
-   )
+   return <div>health-check-js11</div>
 }
 
 export default App
