b4f9ed602deba364dc03493b0ebbcf5b6decbf26
Zamanbekova Nurana <nuranazamanbekova2@gmail.com>
2024-01-12 18:25:56 +0600

    change app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import './App.css'
-import Button from './components/UI/Button'
 
 function App() {
-   return (
-      <div>
-         health-check-js11 <Button />
-      </div>
-   )
+   return <div>health-check-js11</div>
 }
 
 export default App
