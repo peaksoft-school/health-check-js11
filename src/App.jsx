d22a0e4cc691bac48d4e23bed933ab11aa593bec
Zamanbekova Nurana <nuranazamanbekova2@gmail.com>
2024-01-13 18:33:53 +0600

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css'
+import WhyWe from './components/whyWe/WhyMeCard'
 
 function App() {
-   return <div>health-check-js11\</div>
+   return (
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+         <WhyWe />
+      </div>
+   )
 }
 
 export default App
---
+++ src/components/whyWe/WhyMeCard.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import { Typography, Grid, styled } from '@mui/material'
+import { data } from '../../utils/constants/index'
+
+const WhyWe = () => {
+   return (
+      <StyledMainContainer>
+         <StyledMainText>
+            Почему <Typography variant="p">нас выбирают? </Typography>
+         </StyledMainText>
+         <StyledContainerGrid container spacing={2} direction="row">
+            {data.map((item) => (
+               <StyledGrid key={item.key}>
+                  <StyledNumber variant="h1">{item.number}</StyledNumber>
+                  <StyledWhy variant="h5">{item.title}</StyledWhy>
+                  <StyledText variant="body2">{item.text}</StyledText>
+               </StyledGrid>
+            ))}
+         </StyledContainerGrid>
+      </StyledMainContainer>
+   )
+}
+
+export default WhyWe
+
+const StyledMainContainer = styled('div')(() => ({
+   marginLeft: '5%',
+   marginRight: '5%',
+   width: '90%', // Изменил на 90% для уменьшения ширины и обеспечения стабильности
+   margin: 'auto', // Центрируем компонент на странице
+}))
+
+const StyledContainerGrid = styled(Grid)({
+   paddingTop: '3.75%',
+   display: 'flex',
+   justifyContent: 'space-between',
+})
+
+const StyledGrid = styled(Grid)(({ theme }) => ({
+   backgroundColor: theme.palette.tertiary.main,
+   width: '30%',
+   height: '20%',
+   padding: '2.5%',
+   borderRadius: '0.25%',
+   boxSizing: 'border-box', // Используйте box-sizing для учета отступов и рамок в размерах
+}))
+
+const StyledMainText = styled(Typography)(({ theme }) => ({
+   fontFamily: 'Manrope',
+   fontSize: '2.25rem',
+   fontStyle: 'normal',
+   fontWeight: '600',
+   lineHeight: 'normal',
+   '.MuiTypography-root': {
+      color: theme.palette.primary.darkGreen,
+   },
+}))
+
+const StyledNumber = styled(Typography)(({ theme }) => ({
+   fontFamily: 'Manrope',
+   fontSize: '3rem',
+   fontStyle: 'normal',
+   fontWeight: '600',
+   lineHeight: 'normal',
+   color: theme.palette.primary.darkGreen,
+}))
+
+const StyledWhy = styled(Typography)(({ theme }) => ({
+   color: theme.palette.primary.lightBlack,
+   fontSize: '1.25rem',
+   fontStyle: 'normal',
+}))
+
+const StyledText = styled(Typography)({
+   fontFamily: 'Manrope',
+   fontSize: '1rem',
+   fontStyle: 'normal',
+   fontWeight: '400',
+   lineHeight: '130%',
+   paddingTop: '1.125rem',
+})
