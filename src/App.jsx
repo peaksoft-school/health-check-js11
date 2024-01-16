6c0a209d2a4a22b5057b1c6d8af387b1327d6ceb
Zamanbekova Nurana <nuranazamanbekova2@gmail.com>
2024-01-16 15:25:17 +0600

    changed best doctors

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import './App.css'
-// eslint-disable-next-line import/no-unresolved, import/no-extraneous-dependencies
-import Select from 'react-select'
 
 function App() {
-   return (
-      <div>
-         health-check-js11
-         <Select />
-      </div>
-   )
+   return <div>health-check-js11</div>
 }
 
 export default App
