48164bc6ceedcd278fa852fcf719e4a0c7a3be2a
Zamanbekova Nurana <nuranazamanbekova2@gmail.com>
2024-01-19 15:49:52 +0600

    save 19.01

--- src/components/UI/Input/Input.jsx
+++ src/components/UI/Input/Input.jsx
@@ -1,50 +1,41 @@
-import { TextField } from '@mui/material'
-import { styled } from '@mui/material/styles'
 import { forwardRef } from 'react'
+import { TextField, styled } from '@mui/material'
 
 const Input = forwardRef(
-   ({ value, onChange, placeholder, error, type = 'text', ...rest }, ref) => {
-      return (
-         <InputStyled
-            value={value}
-            onChange={onChange}
-            placeholder={placeholder}
-            error={error}
-            ref={ref}
-            type={type}
-            {...rest}
-         />
-      )
-   }
+   ({ type, label, placeholder, onChange, onClick, value, ...rest }, ref) => (
+      <StyledInput
+         type={type}
+         label={label}
+         value={value}
+         onChange={onChange}
+         placeholder={placeholder}
+         ref={ref}
+         {...rest}
+      />
+   )
 )
-export default Input
 
-const InputStyled = styled(TextField)(() => ({
-   '.MuiOutlinedInput-root': {
-      height: '42px',
-      width: '414px',
-      fontFamily: 'Manrope',
-      borderRadius: '8px',
-
+const StyledInput = styled(TextField)(({ theme }) => ({
+   width: '414px',
+   height: '42px',
+   borderRadius: '1px solid',
+   '& .MuiOutlinedInput-input': {
+      borderRadius: '2px',
+   },
+   '& .MuiOutlinedInput-root': {
       '& fieldset': {
-         borderColor: '#D9D9D9',
+         borderColor: theme.palette.secondary.main,
       },
-
       '&:hover fieldset': {
-         borderColor: '#959595',
+         borderColor: theme.palette.secondary.lightGrey,
       },
-
-      '&:active fieldset': {
-         borderColor: '#rgba(4, 135, 65, 0.80)',
-      },
-
-      '&.css-9ddj71-MuiInputBase-root-MuiOutlinedInput-root.Mui-disabled .MuiOutlinedInput-notchedOutline ':
-         {
-            border: '1px solid #F91515',
-         },
-
       '&.Mui-focused fieldset': {
-         borderColor: 'rgba(4, 135, 65, 0.80)',
+         borderColor: theme.palette.primary.darkGreen,
+      },
+      '&.Mui-disabled .MuiOutlinedInput-notchedOutline': {
+         borderColor: theme.palette.tertiary.red,
       },
    },
 }))
+
+export default Input
