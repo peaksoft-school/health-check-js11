635b6c07bb531dd1a3550544df3816e5bc902692
BayturPS <bayturazarbekov@gmail.com>
2024-01-19 10:52:10 +0600

    finish

--- src/components/LeaveRequest.jsx
+++ src/components/LeaveRequest.jsx
@@ -1,57 +1,63 @@
 import { InputBase, Paper, styled } from '@mui/material'
 import Button from './UI/Button'
 import { ActivePhoneIcon, CircleArrow, UserIcon } from '../assets/icons'
-import RequairDoctorImage from '../assets/images/requairDoctor.png'
+import { DoctorRequestImage } from '../assets/images/index'
 
-const LeaveRequest = () => {
-   return (
-      <StyledLeaveRequest>
-         <Container>
-            <StyledTitle>Оставьте заявку</StyledTitle>
-            <StyledDesc>
-               Оставьте свой номер и наши специалисты свяжутся с Вами в
-               ближайшее время
-            </StyledDesc>
-            <AllInputsContainer>
-               <InputContainer>
-                  <StyledLabel htmlFor="name">
-                     Как к Вам обратиться?
-                  </StyledLabel>
-                  <StyledPaper>
-                     <StyledUserIcon />
-                     <StyledInput
-                        id="name"
-                        type="text"
-                        placeholder="
+const LeaveRequest = () => (
+   <StyledLeaveRequest>
+      <Container>
+         <h2 className="title">Оставьте заявку</h2>
+
+         <h2 className="description">
+            Оставьте свой номер и наши специалисты свяжутся с Вами в ближайшее
+            время
+         </h2>
+
+         <AllInputsContainer>
+            <InputContainer>
+               <label htmlFor="name" className="input-label">
+                  Как к Вам обратиться?
+               </label>
+
+               <StyledPaper>
+                  <UserIcon className="input-icons" />
+
+                  <StyledInput
+                     id="name"
+                     type="text"
+                     placeholder="
                      Введите
                      имя"
-                     />
-                  </StyledPaper>
-               </InputContainer>
-               <InputContainer>
-                  <StyledLabel htmlFor="number">
-                     Номер мобильного телефона
-                  </StyledLabel>
-                  <StyledPaper>
-                     <StyledPhoneIcon />
-                     <StyledInput
-                        id="number"
-                        type="number"
-                        placeholder="
-                     +996 (___) __-__-__"
-                     />
-                  </StyledPaper>
-               </InputContainer>
-            </AllInputsContainer>
+                  />
+               </StyledPaper>
+            </InputContainer>
 
-            <StyledButton>
-               ОТПРАВИТЬ ЗАЯВКУ <StyledRightArrow />
-            </StyledButton>
-         </Container>
-         <StyledImage src={RequairDoctorImage} alt="doctor img" />
-      </StyledLeaveRequest>
-   )
-}
+            <InputContainer>
+               <label htmlFor="number" className="input-label">
+                  Номер мобильного телефона
+               </label>
+
+               <StyledPaper>
+                  <ActivePhoneIcon className="input-icons" />
+
+                  <StyledInput
+                     id="number"
+                     type="number"
+                     placeholder="
+                     +996 (___) __-__-__"
+                  />
+               </StyledPaper>
+            </InputContainer>
+         </AllInputsContainer>
+
+         <StyledButton>
+            ОТПРАВИТЬ ЗАЯВКУ <CircleArrow className="circle-arrow" />
+         </StyledButton>
+      </Container>
+
+      <StyledImage src={DoctorRequestImage} alt="doctor img" />
+   </StyledLeaveRequest>
+)
 
 export default LeaveRequest
 
@@ -60,6 +66,7 @@ const StyledLeaveRequest = styled('div')(({ theme }) => ({
    margin: '3.125rem',
    justifyContent: 'center',
    alignItems: 'flex-end',
+
    [theme.breakpoints.down('lg')]: {
       margin: '0.625rem',
    },
@@ -68,6 +75,7 @@ const StyledLeaveRequest = styled('div')(({ theme }) => ({
 const StyledImage = styled('img')(({ theme }) => ({
    width: '36.375rem',
    height: '33.125rem',
+
    [theme.breakpoints.down('lg')]: {
       width: '25rem',
       height: '23rem',
@@ -78,46 +86,38 @@ const Container = styled('form')(({ theme }) => ({
    display: 'flex',
    width: '41.1875rem',
    height: '28.75rem',
-   flexShrink: '0',
    flexDirection: 'column',
    alignItems: 'center',
    justifyContent: 'center',
    textAlign: 'center',
    backgroundColor: '#DBEBFF',
    borderRadius: '1.25rem',
+
    [theme.breakpoints.down('lg')]: {
       height: '21rem',
       width: '33rem',
    },
-}))
 
-const StyledLabel = styled('label')(({ theme }) => ({
-   [theme.breakpoints.down('lg')]: {
-      fontSize: '0.813rem',
+   '& .title': {
+      fontSize: '2.25rem',
+      fontWeight: '500',
+      marginBottom: '1.25rem',
+
+      [theme.breakpoints.down('lg')]: {
+         fontSize: '1.25rem',
+         marginBottom: '0.375rem',
+      },
    },
-}))
 
-const StyledTitle = styled('h2')(({ theme }) => ({
-   fontSize: '2.25rem',
-   fontStyle: 'normal',
-   fontWeight: '500',
-   lineHeight: 'normal',
-   marginBottom: '1.25rem',
-   [theme.breakpoints.down('lg')]: {
-      fontSize: '1.25rem',
-      marginBottom: '0.375rem',
-   },
-}))
+   '& .description': {
+      fontSize: '1.125rem',
+      fontWeight: '400',
+      width: '32.375rem',
 
-const StyledDesc = styled('h2')(({ theme }) => ({
-   fontSize: '1.125rem',
-   fontStyle: 'normal',
-   fontWeight: '400',
-   lineHeight: 'normal',
-   width: '32.375rem',
-   [theme.breakpoints.down('lg')]: {
-      width: '26rem',
-      fontSize: '0.9rem',
+      [theme.breakpoints.down('lg')]: {
+         width: '26rem',
+         fontSize: '0.9rem',
+      },
    },
 }))
 
@@ -128,16 +128,15 @@ const StyledButton = styled(Button)(({ theme }) => ({
       padding: '0.5rem 0.75rem 0.5rem 1.5rem',
       justifyContent: 'space-between',
       height: '3.1875rem',
-      flexShrink: '0',
       fontSize: '0.875rem',
-      fontStyle: 'normal',
-      lineHeight: 'normal',
       textTransform: 'uppercase',
       width: '13.5625rem',
+
       '&:hover': {
          padding: '0.5rem 0.75rem 0.5rem 1.5rem',
          background: theme.palette.primary.linearGradient,
       },
+
       [theme.breakpoints.down('lg')]: {
          width: '11.5rem',
          height: '2.5rem',
@@ -145,11 +144,11 @@ const StyledButton = styled(Button)(({ theme }) => ({
          padding: '0.5rem 0.50rem 0.5rem 1.4rem',
       },
    },
-}))
 
-const StyledRightArrow = styled(CircleArrow)(() => ({
-   width: '1.625rem',
-   height: '1.625rem',
+   '& .circle-arrow': {
+      width: '1.625rem',
+      height: '1.625rem',
+   },
 }))
 
 const AllInputsContainer = styled('div')(() => ({
@@ -159,17 +158,23 @@ const AllInputsContainer = styled('div')(() => ({
    marginBottom: '2.5rem',
 }))
 
-const InputContainer = styled('div')(() => ({
+const InputContainer = styled('div')(({ theme }) => ({
    display: 'flex',
    alignItems: 'flex-start',
    flexDirection: 'column',
+
+   '& .input-label': {
+      [theme.breakpoints.down('lg')]: {
+         fontSize: '0.813rem',
+      },
+   },
 }))
 
 const StyledInput = styled(InputBase)(({ theme }) => ({
-   flexShrink: '0',
    borderRadius: '0.3125rem',
    background: ' #FFF',
    padding: '0px 0.625rem px',
+
    [theme.breakpoints.down('lg')]: {
       width: '10rem',
       fontSize: '0.85rem',
@@ -183,22 +188,15 @@ const StyledPaper = styled(Paper)(({ theme }) => ({
    alignItems: 'center',
    border: '1px solid rgba(0, 147, 68, 0.50)',
    marginTop: '0.375rem',
+
    [theme.breakpoints.down('lg')]: {
       width: '13.4375rem',
       height: ' 2.3rem',
    },
-}))
 
-const StyledUserIcon = styled(UserIcon)(() => ({
-   margin: '0.75rem 0.5rem 0.875rem 0.75rem',
-   width: '1.125rem',
-   height: '1rem',
-   flexShrink: '0',
-}))
-
-const StyledPhoneIcon = styled(ActivePhoneIcon)(() => ({
-   margin: '0.75rem 0.5rem 0.875rem 0.75rem',
-   width: '1.125rem',
-   height: '1rem',
-   flexShrink: '0',
+   '& .input-icons': {
+      margin: '0.75rem 0.5rem 0.875rem 0.75rem',
+      width: '1.125rem',
+      height: '1rem',
+   },
 }))
