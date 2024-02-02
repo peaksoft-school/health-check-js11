f1e5192e36a6c425b1a62c277cc66910287a0dfa
Zamanbekova Nurana <nuranazamanbekova2@gmail.com>
2024-02-02 20:14:17 +0600

    clean app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
 import './App.css'
-import AdminHeader from './layout/AdminHeader'
-import Header from './layout/Header'
 
 const App = () => {
-   return (
-      <div>
-         {/* health-check-js11 */}
-         <AdminHeader />
-         {/* <Header /> */}
-      </div>
-   )
+   return <div>health-check-js11</div>
 }
 
 export default App
